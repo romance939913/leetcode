fb50afa94e66fc4aec1f8e78be5b9a099cfbfb84
Brennan Romance <brennan.romance@gmail.com>
2020-05-04 18:44:59 -0400

    skipping problem1389

--- problem1389.rb
+++ problem1389.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 # Given two arrays of integers nums and index. Your task is to create target array under the following rules:
 
 # Initially target array is empty.
@@ -38,12 +39,28 @@
 
 ##################### first try: SHIT TIME COMPLEXITY #########################
 def create_target_array(nums, index)
-  newIndices = []
+  indexes = []
+  hash = {}
   (0...index.length).each do |i|
-
+    if hash[index[i]]
+      (0...i).each do |j|
+        if indexes[j] >= index[i]
+          indexes[j] += 1
+        end
+      end
+    end
+    hash[index[i]] = 0
+    indexes.push(index[i])
   end
+  answer = Array.new(index.length, 0)
+  j = 0
+  indexes.each do |idx|
+    answer[idx] = nums[j]
+    j += 1
+  end
+  answer
 end
 
 nums = [0,1,2,3,4]
 index = [0,1,2,2,1]
-puts create_target_array(nums, index) # Output: [0,4,1,3,2]
+p create_target_array(nums, index) # Output: [0,4,1,3,2]
