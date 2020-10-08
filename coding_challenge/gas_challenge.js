6e4066366137b95688b734d8c396b3195bb09054
Brennan <brennan.romance@gmail.com>
2020-10-08 12:43:23 -0400

    solve gas required problem in coding_challenges

--- coding_challenge/gas_challenge.js
+++ coding_challenge/gas_challenge.js
@@ -1,35 +1,35 @@
-// left side of ratio is gas, and right side is required gas to reach next stop
+// left side of ratio is the gas you receive at that stop
+// right side of ratio is required gas to reach next stop
 // return the index where you can circle through the array
+// if it's impossible return the string 'impossible'
 
-// let Input = ["4","1:1","2:2","1:2","0:1"]
-// let Output = impossible
-// let Input2 = ["4","0:1","2:2","1:2","3:1"]
-// let Output2 = 4
+let input = ["4","1:1","2:2","1:2","0:1"] // => 'impossible'
+let input2 = ["4","0:1","2:2","1:2","3:1"] // => 4
 
-function ArrayChallenge(strArr) { 
-    let arr = strArr.slice(1);
-    let tries = 0;
-  
-    while (tries <= 4) {
-      let gas = 0;
-      let i = 0
-      while (i < arr.length - 1) {
-        if (i > 4) return 'impossible';
-        let both = arr[i].split(":");
-        gas += parseInt(both[0]);
-        gas -= parseInt(both[1]);
-        if (gas < 0) {
-          tries ++;
-          gas = 0; 
-          let newLast = arr.shift();
-          arr.push(newLast);
-        } else {
-          i++
-        }
-      }
+function gasChallenge(strArr) { 
+  let arr = strArr.slice(1);
+  let tries = 0;  
+  let gas = 0;
+  let i = 0;
+
+  while (i <= arr.length - 1 && tries < 4) {
+    let both = arr[i].split(":");
+    gas += parseInt(both[0]);
+    gas -= parseInt(both[1]);
+    if (gas < 0) {
+      tries++;
+      gas = 0;
+      i = 0; 
+      let newLast = arr.shift();
+      arr.push(newLast);
+    } else {
+      i++
     }
-    return tries;
   }
+
+  return tries < 4 ? tries + 1 : 'impossible';
+}
      
-  // keep this function call here 
-  console.log(ArrayChallenge(readline()));
+// keep this function call here 
+console.log(gasChallenge(input));
+console.log(gasChallenge(input2));
